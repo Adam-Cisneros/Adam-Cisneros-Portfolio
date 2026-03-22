e2b0c6dabd0fde8b5a48f0a9df514a7466300e95
Adam-Cisneros <111794724+Adam-Cisneros@users.noreply.github.com>
2026-03-22 12:35:22 -0700

    deployed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/adam-cisneros-portfolio/',
+  base: '/adam-cisneros-portfolio',
   plugins: [
     tailwindcss(),
     react()
